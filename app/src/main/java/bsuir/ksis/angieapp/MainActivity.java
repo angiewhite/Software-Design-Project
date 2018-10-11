241a13560bf2fb49e51839f32611e4b70d809a75
Angelina Belyasova <belyasova98@mail.ru>
2018-10-11 23:38:58 +0300

    Basic permission mechanism implementation

--- app/src/main/java/bsuir/ksis/angieapp/MainActivity.java
+++ app/src/main/java/bsuir/ksis/angieapp/MainActivity.java
@@ -1,6 +1,11 @@
 package bsuir.ksis.angieapp;
 
-import android.app.Activity;
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.telephony.TelephonyManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -12,6 +17,48 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TextView imei_tv = findViewById(R.id.imei_text_view);
-        imei_tv.setText("lkfjdlks");
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_PHONE_STATE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_PHONE_STATE},
+                    1);
+        } else {
+            TelephonyManager manager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+            String imei = manager.getDeviceId();
+            imei_tv.setText(imei);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        TextView imei_tv = findViewById(R.id.imei_text_view);
+        switch (requestCode) {
+            case 1: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    TelephonyManager manager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+                    try {
+                        String imei = manager.getDeviceId();
+                        imei_tv.setText(imei);
+                    } catch (SecurityException e) {
+
+                    }
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+                    imei_tv.setText("You didn't let me fetch the imei :(");
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
     }
 }
