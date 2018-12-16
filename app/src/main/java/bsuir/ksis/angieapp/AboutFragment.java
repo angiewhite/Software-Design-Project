bc8342f760489b3319df2106d85b92dfb0a2e23d
angiewhite <belyasova98@mail.ru>
2018-12-16 21:14:29 +0300

    Deep links

--- app/src/main/java/bsuir/ksis/angieapp/AboutFragment.java
+++ app/src/main/java/bsuir/ksis/angieapp/AboutFragment.java
@@ -1,21 +1,95 @@
 package bsuir.ksis.angieapp;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
-import android.net.Uri;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
-
+import android.telephony.TelephonyManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import com.google.android.material.snackbar.Snackbar;
 
 public class AboutFragment extends Fragment {
+
+    private static final int REQUEST_READ_PHONE_STATE = 78;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_about, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        displayVersion();
+
+        if (ActivityCompat.checkSelfPermission(getActivity(),
+                Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            requestReadPhoneStatePermission();
+            if (ActivityCompat.checkSelfPermission(getActivity(),
+                    Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED) {
+                displayIMEI();
+            }
+        } else {
+            displayIMEI();
+        }
+    }
+
+    private void displayVersion()
+    {
+        TextView textView = getActivity().findViewById(R.id.version_text_view);
+        textView.setText(BuildConfig.VERSION_NAME);
+    }
+
+    private void displayIMEI()
+    {
+        Activity activity = getActivity();
+
+        TextView imei_tv = activity.findViewById(R.id.imei_text_view);
+        TelephonyManager manager = (TelephonyManager)activity.getSystemService(Context.TELEPHONY_SERVICE);
+
+        String imei;
+        try {
+            imei = manager.getDeviceId();
+            imei_tv.setText(imei == null ? getString(R.string.IMEI, getString(R.string.unknown)) :
+                    getString(R.string.IMEI, imei));
+        } catch (SecurityException e) {
+            throw e;
+        }
+
+        TextView textView = getActivity().findViewById(R.id.imei_text_view);
+        textView.setText(imei == null ? getString(R.string.IMEI, getString(R.string.unknown)) :
+                                        getString(R.string.IMEI, imei));
+    }
+
+    private void requestReadPhoneStatePermission() {
+        final Activity activity = getActivity();
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(activity,
+                Manifest.permission.READ_PHONE_STATE)) {
+            String message = "Give Phone permission to the app to enable IMEI display functionality.";
+            Snackbar.make(activity.findViewById(R.id.parent_container), message, Snackbar.LENGTH_INDEFINITE)
+            .setAction(R.string.snackbar_action, new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    ActivityCompat.requestPermissions(activity,
+                            new String[]{Manifest.permission.READ_PHONE_STATE},
+                            REQUEST_READ_PHONE_STATE);
+                }
+            }).show();
+        } else {
+            ActivityCompat.requestPermissions(activity,
+                    new String[]{Manifest.permission.READ_PHONE_STATE},
+                    REQUEST_READ_PHONE_STATE);
+        }
     }
 }
--- app/src/main/java/bsuir/ksis/angieapp/HomeActivity.java
+++ app/src/main/java/bsuir/ksis/angieapp/HomeActivity.java
@@ -1,19 +1,15 @@
 package bsuir.ksis.angieapp;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.provider.Settings;
 import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import android.telephony.TelephonyManager;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -21,16 +17,14 @@ import android.view.View;
 import android.widget.TextView;
 import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.ui.NavigationUI;
-
-import com.google.android.material.bottomnavigation.BottomNavigationMenu;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.snackbar.Snackbar;
 
 public class HomeActivity extends AppCompatActivity {
 
-//    private static final int REQUEST_READ_PHONE_STATE = 78;
-//
-//    private boolean wasStopped = false;
+    private static final int REQUEST_READ_PHONE_STATE = 78;
+
+    private boolean wasStopped = false;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,17 +36,6 @@ public class HomeActivity extends AppCompatActivity {
 
         setupBottomNavMenu(navController);
         setupActionBar(navController);
-
-//        TextView version_tv = findViewById(R.id.version_text_view);
-//
-//        version_tv.setText(BuildConfig.VERSION_NAME);
-//
-//        if (ContextCompat.checkSelfPermission(this,
-//                Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
-//            requestReadPhoneStatePermission();
-//        } else {
-//            tryShowIMEI();
-//        }
     }
 
     private void setupBottomNavMenu(NavController navController)
@@ -65,7 +48,7 @@ public class HomeActivity extends AppCompatActivity {
     private void setupActionBar(NavController navController)
     {
         DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
-        NavigationUI.setupActionBarWithNavController(this, navController);
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout);
     }
 
     @Override
@@ -76,6 +59,7 @@ public class HomeActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+
         NavController navController = Navigation.findNavController(this, R.id.fragment);
         boolean navigated = NavigationUI.onNavDestinationSelected(item, navController);
         return navigated || super.onOptionsItemSelected(item);
@@ -88,90 +72,38 @@ public class HomeActivity extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, drawerLayout);
     }
 
-    //    protected void onStop() {
-//        super.onStop();
-//
-//        wasStopped = true;
-//    }
-//
-//    protected void onResume() {
-//        super.onResume();
-//
-//        if (!wasStopped) return;
-//        wasStopped = false;
-//
-//        if (ContextCompat.checkSelfPermission(this,
-//                Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
-//            requestReadPhoneStatePermission();
-//        } else {
-//            tryShowIMEI();
-//        }
-//    }
-//
-//    private void requestReadPhoneStatePermission() {
-//        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-//                Manifest.permission.READ_PHONE_STATE)) {
-//            String message = "Give Phone permission to the app to enable IMEI display functionality.";
-//            Snackbar.make(findViewById(R.id.parent_container), message, Snackbar.LENGTH_INDEFINITE)
-//            .setAction(R.string.snackbar_action, new View.OnClickListener() {
-//                @Override
-//                public void onClick(View view) {
-//                    ActivityCompat.requestPermissions(HomeActivity.this,
-//                            new String[]{Manifest.permission.READ_PHONE_STATE},
-//                            REQUEST_READ_PHONE_STATE);
-//                }
-//            }).show();
-//        } else {
-//            ActivityCompat.requestPermissions(this,
-//                    new String[]{Manifest.permission.READ_PHONE_STATE},
-//                    REQUEST_READ_PHONE_STATE);
-//        }
-//    }
-//
-//    private void tryShowIMEI() {
-//        TextView imei_tv = findViewById(R.id.imei_text_view);
-//        TelephonyManager manager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
-//        try {
-//            String imei = manager.getDeviceId();
-//            imei_tv.setText(imei == null ? getString(R.string.IMEI, getString(R.string.unknown)) :
-//                    getString(R.string.IMEI, imei));
-//        } catch (SecurityException e) {
-//            throw e;
-//        }
-//    }
-//
-//    private void openSettingsForPhoneStatePermission(){
-//        Intent appSettingsIntent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
-//                Uri.fromParts("package", getPackageName(), null));
-//        startActivityForResult(appSettingsIntent, REQUEST_READ_PHONE_STATE);
-//    }
-//
-//    @Override
-//    public void onRequestPermissionsResult(int requestCode,
-//                                           String permissions[], int[] grantResults) {
-//        switch (requestCode) {
-//            case REQUEST_READ_PHONE_STATE: {
-//                if (grantResults.length > 0
-//                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//                    tryShowIMEI();
-//                } else {
-//                    TextView imei_tv = findViewById(R.id.imei_text_view);
-//                    imei_tv.setText(getString(R.string.imei_fail));
-//                    if (!ActivityCompat.shouldShowRequestPermissionRationale(this,
-//                            Manifest.permission.READ_PHONE_STATE)) {
-//                        String message = "You can grant the permission later in app settings (Permissions section)";
-//                        Snackbar.make(findViewById(R.id.parent_container), message, Snackbar.LENGTH_LONG)
-//                                .setAction("SETTINGS", new View.OnClickListener() {
-//                                    @Override
-//                                    public void onClick(View view) {
-//                                        openSettingsForPhoneStatePermission();
-//                                    }
-//                                })
-//                                .show();
-//                    }
-//                }
-//                break;
-//            }
-//        }
-//    }
+    private void openSettingsForPhoneStatePermission(){
+        Intent appSettingsIntent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                Uri.fromParts("package", getPackageName(), null));
+        startActivityForResult(appSettingsIntent, REQUEST_READ_PHONE_STATE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_READ_PHONE_STATE: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                } else {
+                    TextView imei_tv = findViewById(R.id.imei_text_view);
+                    imei_tv.setText(getString(R.string.imei_fail));
+                    if (!ActivityCompat.shouldShowRequestPermissionRationale(this,
+                            Manifest.permission.READ_PHONE_STATE)) {
+                        String message = "You can grant the permission later in app settings (Permissions section)";
+                        Snackbar.make(findViewById(R.id.parent_container), message, Snackbar.LENGTH_LONG)
+                                .setAction("SETTINGS", new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View view) {
+                                        openSettingsForPhoneStatePermission();
+                                    }
+                                })
+                                .show();
+                    }
+                }
+                break;
+            }
+        }
+    }
 }
